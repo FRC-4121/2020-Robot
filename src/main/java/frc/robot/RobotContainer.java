4e98d7f53081e72426fe49900165960d787a7450
JonasMuhlenkamp <jonasmuhlenkamp@gmail.com>
2020-02-04 23:44:05 -0500

    create prototype network tables subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,10 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.AutoDrive;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.IntakeAndProcessing;
+import frc.robot.subsystems.NetworkTableQuerier;
+import frc.robot.subsystems.Pneumatics;
+import frc.robot.subsystems.Shooter;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -20,11 +24,16 @@ import frc.robot.subsystems.Drivetrain;
  * (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+  
+  //Subsystems
   private final Drivetrain drivetrain = new Drivetrain();
+  private final Shooter shooter = new Shooter();
+  private final IntakeAndProcessing processor = new IntakeAndProcessing();
+  private final Pneumatics pneumatics = new Pneumatics();
+  private final NetworkTableQuerier ntables = new NetworkTableQuerier();
 
+  //Commands
   private final AutoDrive autoCommand = new AutoDrive(drivetrain);
-
 
 
   /**
--- src/main/java/frc/robot/subsystems/NetworkTableQuerier.java
+++ src/main/java/frc/robot/subsystems/NetworkTableQuerier.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -9,24 +9,20 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Drivetrain extends SubsystemBase {
-  
-  
-  
-  public Drivetrain() {
+public class NetworkTableQuerier extends SubsystemBase {
+  /**
+   * A software-only subsystem designed to assist with network table communication between RPi and Rio 
+   * after the 2020 structure updates
+   * 
+   * Instructions:
+   * Create all values outside of the constructor
+   */
+  public NetworkTableQuerier() {
 
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-  }
-
-  public void drive(double LeftJoyX, double LeftJoyY, double RightJoyX, double RightJoyY) {
-    
-  }
-
-  public void autoDrive(double leftSpeed, double rightSpeed){
-    
   }
 }
